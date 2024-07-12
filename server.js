8a51d49f98760a6945d359f93354576c06d586cb
Rami Abdelqader <ramiabdelqader@Ramis-MBP.lan>
2024-07-12 18:33:15 -0500

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001; // or any other port you prefer
 
-app.get('/favourites', (req, res) => {
+app.get('/favourite', (req, res) => {
   // Handle your API logic here
   res.set('Access-Control-Allow-Origin', '*');
   const data = { message: 'Hello from the server!' };
