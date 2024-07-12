331db80a740640c99ca01f1b169b848fbbdd284c
Rami Abdelqader <ramiabdelqader@Ramis-MBP.lan>
2024-07-12 18:15:58 -0500

    commit

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3001; // or any other port you prefer
+const port = process.env.PORT || 3001; // or any other port you prefer
 
 app.get('/favourites', (req, res) => {
   // Handle your API logic here
   const data = { message: 'Hello from the server!' };
   res.json(data);
+});
+
+app.get('/', (req, res) => {
+  // Handle your API logic here
+  res.send("hello world")
 });
 
 app.listen(port, () => {
